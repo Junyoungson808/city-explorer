b627881bdadb06404c6e993129c0262f7f343d41
Junyoung Son <junyoungson808@gmail.com>
2022-08-23 16:31:26 -0700

    lab6

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
+### Time Estimates
+For each of the lab features, make an estimate of the time it will take you to complete the feature, and record your start and finish times for that feature:
+
+Name of feature: __Lab-06:_City-Explorer__
+
+Estimate of time needed to complete: _5 hrs_
+
+Start time: _8:00pm_
+
+Finish time: _N/A_
+
+Actual time needed to complete: _____
+
+Name of feature: __Lab-07:_City-Explorer__
+
+Estimate of time needed to complete: _5 hrs_
+
+Start time: _8:00pm_
+
+Finish time: _N/A_
+
+Actual time needed to complete: _____
+
+
+
 # City-Exploerer
 
 **Author**: Junyoung Son
 **Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+[netlify](https://chimerical-macaron-5cd1fe.netlify.app/)
+[axios]()
 
 ## Overview
 <!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
@@ -19,17 +46,5 @@
 
 ## Credit and Collaborations
 <!-- Give credit (and a link) to other people or resources that helped you build this application. -->
-Time Estimates
-For each of the lab features, make an estimate of the time it will take you to complete the feature, and record your start and finish times for that feature:
-
-Name of feature: ________________________________
-
-Estimate of time needed to complete: _____
-
-Start time: _____
-
-Finish time: _____
-
-Actual time needed to complete: _____
 
 ![Lab6 whiteboard](Lab6%20-%20City%20Explorer%20Whiteboard.jpg)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div >
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemonData: [],
+      city: "",
+      cityData: [],
+      // searchResult: null,
+      // searchQuery: "",
+      // mapURL: "",
+      // Error: false,
+    };
+  }
 
-    </div>
-  );
+  handleInput = (e) => {
+    e.preventDefault();
+    this.setState({
+      city: e.target.value,
+    });
+  };
+
+  getCityData = async (e) => {
+    e.preventDefault();
+
+    let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+
+    let cityData = await axios.get(url);
+
+    console.log(cityData.data[0]);
+  };
+
+  handleGetPokemon = async (e) => {
+    e.preventDefault();
+    // first axios call-url
+    let pokemonData = await axios.get("https://pokeapi.co/api/v2/pokemon");
+    // proof of life
+    console.log(pokemonData.data.results);
+    this.setState({
+      pokemonData: pokemonData.data.results,
+    });
+  };
+
+  render() {
+    console.log("app.state: ", this.state);
+
+    let pokemonItems = this.state.pokemonData.map((pokemon, index) => {
+      return <li key={index}>{pokemon.name}</li>;
+    });
+
+    return (
+      <>
+        <header className="Header"></header>
+        <h1>Pokemon Data</h1>
+        <form>
+          <button onClick={this.handleGetPokemon}>Gotta Catch Em All!</button>
+        </form>
+
+        <form onSubmit={this.getCityData}>
+          <label>
+            {" "}
+            Pick a City!
+            <input type="text" onInput={this.handleInput} />
+          </label>
+          <button type="submit">Explore!</button>
+        </form>
+
+        <ul>{pokemonItems}</ul>
+      </>
+    );
+  }
 }
 
 export default App;
